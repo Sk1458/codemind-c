f4d84525ff7435bba39e1d49c6bcf76087ea3518
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-06-03 07:03:10 +0530

    #include<stdio.h> #include<math.h> int main() {     float a,b,c,area,s;     scanf("%f %f %f",&a,&b,&c);     s=(a+b+c)/2;     area=pow(s*(s-a)*(s-b)*(s-c),0.5);     printf("%.2f",area);     return 0; }

---
+++ Area_of_Triangle_.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,b,c,area,s;
+    scanf("%f %f %f",&a,&b,&c);
+    s=(a+b+c)/2;
+    area=pow(s*(s-a)*(s-b)*(s-c),0.5);
+    printf("%.2f",area);
+    return 0;
+}
