0c77858109f7d351b599eb586633cd5dee6d2623
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-06-28 07:19:54 +0530

    #include<stdio.h>
    int main()
    {
        int n,m,i=1;
        scanf("%d%d",&n,&m);
        for(;i;i++)
        {
            if((m*i)%n==0)
            {
                printf("%d",m*i);
                break;
            }
        }
        return 0;
    }

---
+++ LCM.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,m,i=1;
+    scanf("%d%d",&n,&m);
+    for(;i;i++)
+    {
+        if((m*i)%n==0)
+        {
+            printf("%d",m*i);
+            break;
+        }
+    }
+    return 0;
+}
