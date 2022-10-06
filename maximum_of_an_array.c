54231cc23f45a5a9b2a8c7854cb2d285ad9f57a3
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-10-06 06:46:35 +0530

    #include<stdio.h> #include<limits.h> int main() {     int n,max=INT_MIN;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(max<arr[i])         max=arr[i];     }     printf("%d",max); }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    int n,max=INT_MIN;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(max<arr[i])
+        max=arr[i];
+    }
+    printf("%d",max);
+}
