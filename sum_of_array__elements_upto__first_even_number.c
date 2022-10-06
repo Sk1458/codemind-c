3532bd67011d4c99f17444ea2250b5816f12d018
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-10-06 12:03:50 +0530

    #include<stdio.h>
    int main()
    {
        int s,i,sum=0;
        scanf("%d",&s);
        int a[s];
        for(i=0;i<s;i++)
        {
            scanf("%d",&a[i]);
            if(a[i]%2!=0)
            {
                sum+=a[i];
            }
            else
            {
                break;
            }
        }
        printf("%d",sum);
        return 0;
    }

---
+++ sum_of_array__elements_upto__first_even_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int s,i,sum=0;
+    scanf("%d",&s);
+    int a[s];
+    for(i=0;i<s;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]%2!=0)
+        {
+            sum+=a[i];
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
