636952b98fe4b5683cca6fcbc73e18fca2ea0bb6
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-10-06 09:21:49 +0530

    #include<stdio.h>
    int main()
    {
        int s,i;
        scanf("%d",&s);
        int a[s];
        for(i=0;i<s;i++)
        {
            scanf("%d",&a[i]);
        }
        for(i=s-1;i>=0;i--)
        {
            if(a[i]%2==0)
            {
                printf("%d",a[i]);
                break;
            }
        }
        return 0;
    }

---
+++ Last_even_number_in_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int s,i;
+    scanf("%d",&s);
+    int a[s];
+    for(i=0;i<s;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=s-1;i>=0;i--)
+    {
+        if(a[i]%2==0)
+        {
+            printf("%d",a[i]);
+            break;
+        }
+    }
+    return 0;
+}
